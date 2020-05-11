c1bbb2aaf22428e29434f054ae09f83aea342446
Chrisclrk9 <chrisclrk9@gmail.com>
2020-05-11 13:03:06 +0100

    introduced ternary operator

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ countryName = document.getElementById("c-id");
 active = document.getElementById("active");
 perDeath = document.getElementById("perDeath");
 todayDeaths = document.querySelector("#todayDeaths");
+recovered = document.querySelector('.recovered');
+
+document.title = "Covid-19 Tracker || Live Data";
 
 function pushData() {
   let country = document.getElementById("countries").value;
-
   fetch(`https://coronavirus-19-api.herokuapp.com/countries/${country}`)
     .then((response) => {
       return response.json();
@@ -14,16 +16,17 @@ function pushData() {
     .catch((err) => alert(err))
     .then((countries) => {
       console.log(countries);
-      // countryName.innerHTML = countries.country;
-      // active.innerHTML = ` Active cases in ${countries.country} <h1 id="casesh1">+${countries.active}</h1>`;
-      // cases.innerHTML = `Total Cases <h1>${countries.cases}</h1>`;
-      // casesToday.innerHTML = `Today's cases in ${countries.country} <h1>${countries.todayCases}</h1>`;
-
-      
+      ifRecovered = countries.recovered === null ? 0 : countries.recovered;
+      console.log(ifRecovered)
       todayDeaths.innerHTML = `Today's deaths in ${countries.country} <h1 >${countries.todayDeaths}</h1>`;
-      deaths.innerHTML = `Today's cases in ${countries.country} <h1 >${countries.todayCases}</h1>`;
-      // active.innerHTML = countries.active;
-      // perDeath.innerHTML =
-      // (countries.deaths * 100) / countries.cases.toFixed(2);
+      deaths.innerHTML = `Today's cases in  ${countries.country} <h1 >${countries.todayCases}</h1>`;
+      active.innerHTML = ` <p>${countries.active}</p>`
+      recovered.innerHTML = ` <p>${ifRecovered}</p>`
+      totalDeaths.innerHTML = `<p>${countries.deaths}</p>`
+
+      document.title = `${countries.country} Covid-19 Tracker || Live Data`
+      
+
     });
 }
+
