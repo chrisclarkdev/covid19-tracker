4cd6e19c45928d3a99fcc8b9a00dd5f0cad0c04b
Chrisclrk9 <chrisclrk9@gmail.com>
2020-05-11 07:48:33 +0100

    added summary section
    cleaned up the text in the cases and death section and summary section

--- app.js
+++ app.js
@@ -15,12 +15,13 @@ function pushData() {
     .then((countries) => {
       console.log(countries);
       // countryName.innerHTML = countries.country;
-      active.innerHTML = ` Active cases in ${countries.country} <h1 id="casesh1">+${countries.active}</h1>`;
-      cases.innerHTML = `Total Cases <h1>${countries.cases}</h1>`;
-      casesToday.innerHTML = `Today's cases in ${countries.country} <h1>${countries.todayCases}</h1>`;
+      // active.innerHTML = ` Active cases in ${countries.country} <h1 id="casesh1">+${countries.active}</h1>`;
+      // cases.innerHTML = `Total Cases <h1>${countries.cases}</h1>`;
+      // casesToday.innerHTML = `Today's cases in ${countries.country} <h1>${countries.todayCases}</h1>`;
 
-      deaths.innerHTML = `Total deaths in ${countries.country} <h1 >${countries.deaths}</h1>`;
+      
       todayDeaths.innerHTML = `Today's deaths in ${countries.country} <h1 >${countries.todayDeaths}</h1>`;
+      deaths.innerHTML = `Today's cases in ${countries.country} <h1 >${countries.todayCases}</h1>`;
       // active.innerHTML = countries.active;
       // perDeath.innerHTML =
       // (countries.deaths * 100) / countries.cases.toFixed(2);
