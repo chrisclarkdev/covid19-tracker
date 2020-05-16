d08d11f02b39926360b35f9a19d22d576b249d7e
Chrisclrk9 <chrisclrk9@gmail.com>
2020-05-16 18:06:38 +0100

    clear input when submit button is pressed

--- app.js
+++ app.js
@@ -10,21 +10,23 @@ critical = document.querySelector('.critical');
 document.title = "Covid-19 Tracker || Live Data";
 
 function pushData() {
-  let country = document.getElementById("countries").value || "UK";
-  fetch(`https://coronavirus-19-api.herokuapp.com/countries/${country}`)
+  let country = document.getElementById("countries"); 
+  countryA = country.value || "UK";
+  fetch(`https://coronavirus-19-api.herokuapp.com/countries/${countryA}`)
     .then((response) => {
       return response.json();
     })
     .catch((err) => console.log(err))
     .then((countries) => {
-      console.log(countries);
+      
       function commaSeparateNumber(val){
         while (/(\d+)(\d{3})/.test(val.toString())){
           val = val.toString().replace(/(\d+)(\d{3})/, '$1'+','+'$2');
-          console.log(val)
         }
         return val;
       }
+      
+      country.value = "";
       commaTested = commaSeparateNumber(countries.totalTests);
       // commaActive = commaSeparateNumber(countries.active);
       commaCritical = commaSeparateNumber(countries.critical);
@@ -34,7 +36,7 @@ function pushData() {
       ifRecovered = countries.recovered === null ? "No Data" : countries.recovered;
       commaRecovered = commaSeparateNumber(ifRecovered)
       commaActive = commaSeparateNumber(ifActive);
-      console.log(commaActive);
+      // console.log(commaActive);
       
       todayDeaths.innerHTML = `Today's deaths in ${countries.country} <h1 >${countries.todayDeaths}</h1>`;
       deaths.innerHTML = `Today's cases in  ${countries.country} <h1 >${countries.todayCases}</h1>`;
